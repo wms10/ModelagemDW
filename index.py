77de996c125f23aa10ed729e0da9582f9f5cb62d
Wanderon Moura <wms10recife@gmail.com>
2024-02-18 21:01:39 -0300

    consultas
    streamlit

---
+++ index.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title('Despesas Orçamentárias de Recife-PE')
+
+st.subheader('Data Warehouse das Despesas Orçamentárias de Recife-PE')
+
+st.markdown("<h3 style='font-size: 16px;'>Wanderson Moura da Silva</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='font-size: 16px;'>Fellipe de Brito Lira Batista</h3>", unsafe_allow_html=True)
+
+
+
+
+# st.markdown('[Pergunta 1](http://localhost:8501/pergunta1)')
+# st.markdown('[Pergunta 2](http://localhost:8501/pergunta2)')
+# st.markdown('[Pergunta 3](http://localhost:8501/pergunta3)')
+# st.markdown('[Pergunta 4](http://localhost:8501/pergunta4)')
