cd210e119112551e2c53aa3417f8323f4a5e202b
Wanderon Moura <wms10recife@gmail.com>
2024-02-19 21:18:58 -0300

    test

--- pages/pergunta1.py
+++ pages/pergunta1.py
@@ -1,18 +1,25 @@
-import mysql.connector
+#import mysql.connector
+
 import pandas as pd
 import streamlit as st
 
 # Conectar ao banco de dados
-connection = mysql.connector.connect(
-    host='35.247.239.74',
-    user='root',
-    password='lQ#>faP@yG{"qMkc',
-    database='dw_populado_recife'
-)
+#connection = mysql.connector.connect(
+    #host='35.247.239.74',
+    #user='root',
+    #password='lQ#>faP@yG{"qMkc',
+    #database='dw_populado_recife'
+#)
 
 # Função para executar consultas e obter resultados em um DataFrame
-def execute_query(query):
-    return pd.read_sql_query(query, connection)
+from sqlalchemy import create_engine, text
+
+# Substitua 'sua_string_de_conexao' pela string de conexão do seu banco de dados
+db_connection = 'mysql+pymysql://wanderson:12345678@35.247.239.74/dw_populado_recife'
+engine = create_engine(db_connection)
+
+# Estabeleça uma conexão
+conn = engine.connect()
 
 # Título do aplicativo
 st.title("Análise OLAP - Data Warehouse")
